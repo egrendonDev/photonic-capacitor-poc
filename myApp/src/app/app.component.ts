1ddff642c7f20d2a8adeadf240f1e6605c997197
Ernesto Rendon <egrendon.dev@gmail.com>
2020-08-31 10:56:10 -0700

    init

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Camera, CameraResultType, CameraSource, Plugins } from '@capacitor/core';
 import { TabsPage } from '../pages/tabs/tabs';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  photo: SafeResourceUrl;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  constructor(platform: Platform, statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private sanitizer: DomSanitizer) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +24,19 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+
+
+
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+  }
+
 }
